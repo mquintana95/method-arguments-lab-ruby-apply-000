8fb5a8e6440f60ea1f0c455dd159792b910ad87d
Marcus Quintana <mq97325@gmail.com>
2020-07-25 19:27:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction(name = "Josh", language = "")
+def introduction(name = "Josh")
   puts "Hi, my name is #{name}."
 end 
+
+def introduction(name, language)
+  puts "Hi my name is #{name} and I am kearning to program in #{language}."
+end
